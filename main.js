27a9ecb66fb1871f48c2bc147dd5ebf335a0bce8
King Arciaga <kingarciaga@gmail.com>
2022-07-29 16:08:59 -0400

    add button that shows current date to index

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
+//generates a random number between 0 and 100
 const getRandomNumber = () => Math.round(Math.random() * 100);
 
+
+
+//add button interactivity
+let dateButton = document.getElementById('button');
+//function to change innerHTML of removeElement to currentDate
+dateButton.onclick = () => {
+    removeElement.innerHTML = currentDate;
+};
+
+
+
+//remove an element from JStexts
+let removeElement = document.getElementById('JStexts').children[1];
+//document.getElementById('JStexts').removeChild(removeElement);
+
+
+
+//create a variable that contains Date obj
 let today = new Date();
+//simple date text
+let currentDate = `${today.getFullYear()} ${today.getMonth()} ${today.getDate()}`; 
 
-let currentDate = `${today.getDate()} ${today.getMonth()} ${today.getFullYear()}`; 
 
-document.getElementById('JStext').innerHTML = currentDate;
 
+//creates a new p element
+let newjsSText = document.createElement('p');
+//sets new element's id to JStext
+newjsSText.id = 'JStext'
+//sets content inside new p 
+newjsSText.innerHTML = currentDate;
+//append new element to parent  
+//document.getElementById('JStexts').appendChild(newjsSText); 
+
+
+
+//add latest p element in JSTest
+let newjsSTextTwo = document.createElement('p');
+newjsSTextTwo.id = 'JStext';
+newjsSTextTwo.innerHTML = 'appended!';
+//document.getElementById('JStexts').appendChild(newjsSTextTwo); 
+
+
+
+//Accessing the first child element of .projects class
+let projectHeader = document.getElementsByClassName('projects')[0].children[0];
+//projectHeader.style.backgroundColor = 'red';
